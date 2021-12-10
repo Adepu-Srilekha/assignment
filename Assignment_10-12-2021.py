39d572d098d10142d3f0cf70ba2b35725d8d463c
Adepu-Srilekha <95116311+Adepu-Srilekha@users.noreply.github.com>
2021-12-10 16:43:22 +0530

    Add files via upload
    10-12-21

---
+++ Assignment_10-12-2021.py
@@ -0,0 +1,243 @@
+'''Python Developer Role:
+
+1.Vanilla Networks private limited
+
+keyskills required:
+ReactJS and NodeJS
+data security
+familiar with API's,JSON,SOAP,REST
+
+Frameworks Required:
+Django,flask
+JS Frameworks
+AngularJS,jQuery,NodeJS
+
+
+
+2.Vee Technologies Private Limited
+---3 years experience
+
+skills:
+MS SQL,my SQL,oracle
+Automation anywhere,Ui path,power automate,Blue
+prism having certification in any will be given high importance.
+
+
+3.Multi Recruit
+key skills:
+ASP.Net,C#,HTML5,MVC
+cloud solutions such as Azure or AWS
+DOCKER
+
+
+
+4.Bigshyft Hiring for Delhivery
+ Atleast 1+years of hands on
+Django
+AWS or cloud platform is preferred
+Docker,Kubernets
+microservice architecture patterns
+
+
+
+5.KoiReader Technologies
+0-5 years experience
+keyskills:
+SQL,NoSQL,NewSQL
+
+Expertise in atleast one popular python framework:
+like FastAPI,Flask,Django,Pyramid
+proficiency in core python
+AWS services like S3
+Build REST/GRPC APIs
+bigdata processing applications using Pubsub
+
+
+6.Squareroot Consulting private Limited
+-------      1-5 years
+Key skills:
+Python,Django,HTML/CSS/JavaScript
+MySQL,Solr,MongoDB,Memcache,Redis,RabbitMQ
+
+
+
+7.b\BRIDGELABS SOLUTIONS PRIVATE LIMITED
+----------- 1-4 YEARS
+
+key skills:
+CSS
+Linux
+Software engineering
+MySQL
+Javascript
+HTML
+Python
+
+
+8.In Time VisionSoft Pvt Ltd
+
+--------     2-5 years
+
+
+knowledge of python :
+packages,module developement,tuning,debugging tools
+python identifiers,reserved words,basic operators,variable types
+user defined exception handling with their usage
+Tuples/List
+create,Edit,Delete view file
+String,DateTime,Numbers and other functions
+
+OOPS concepts
+Django
+JavaScript and jQuery
+RESTFUL webservices
+
+
+
+
+
+
+9.PPN Solutions Pvt Ltd.
+---------3-5 years
+
+key skills:
+python,java,Scala,C++
+
+
+
+
+
+10.Codingmart Technologies
+---------2-5 years
+
+key skills:
+python with Django
+core python
+Javascript
+HTML5
+Mysql
+Agile Methodologies
+GIT,
+oops,
+Data structure
+
+
+
+
+11.Atria Convergence Technologies Private Limited
+--------2-3 years
+
+key skills:
+python3
+Mysql
+Flask
+Django
+REST API's
+Git,Mercurial or SVN
+Agile and waterfall SDLC models
+
+
+
+12.RS Consultants
+-----------------  2-5 years
+
+key skills:
+automation tools
+panda,numoy,matplotlib
+python programming
+class inheritance
+exception handling
+oops concepts
+excel
+yaml and xml file manipulation
+
+
+13.Avantha Technologies Limited
+------ 2-7 years
+
+key skills:
+HTML
+JavaScript,CSS
+oops
+Apache
+Database design and performance tuning
+
+
+14.VN2 SOLUTIONS PRIVATE LIMITED
+-----2-7 YEARS
+
+KEY SKILLS:
+Django
+Flask
+Fast/Rest web framework
+Restful APi's
+data structures
+networking
+data security
+AWS
+Docker
+Agile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
