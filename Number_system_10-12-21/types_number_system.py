bd975fced63da9c82f48289103876c40479d7808
Adepu-Srilekha <95116311+Adepu-Srilekha@users.noreply.github.com>
2021-12-10 18:15:32 +0530

    Add files via upload
    10-12-2021

---
+++ Number_system_10-12-21/types_number_system.py
@@ -0,0 +1,107 @@
+'''Number system:
+
+The number system or the numeral system is the system of naming or representing numbers.
+A number system is defined as a system of writing to express numbers.
+The value of any digit in a number can be determined by:
+
+The digit
+Its position in the number
+The base of the number system
+
+
+A number is a mathematical value used for counting or measuring or labelling objects.
+Numbers are used to performing arithmetic calculations.  Examples of numbers are natural numbers, whole numbers,
+rational and irrational numbers, etc. 0 is also a number that represents a null value.
+
+types of numbers:
+
+Natural numbers:
+containing all counting numbers starting from 1.
+example: N={1,2,3,4,.....}
+
+whole numbers:
+It is collection of 0 and natual numbers.
+eg: W={0,1,2,3,4,....}
+
+Integers:
+Collection result of whole numbers and negative of all natural numbers.
+eg:-infinity,......-4,-3,-2,-1,0,1,2,3,4,.........infinity
+
+
+Relational numbers:
+Numbers that can be written in the form of p/q, where q≠0.
+eg:- 5/6,8/3
+
+Irrational numbers:
+All the numbers which are not rational and cannot be written in the form of p/q.
+eg: Irrational numbers are non-terminating and non-repeating in nature like √2.
+
+Even numbers:
+even numbers are those numbers that are exactly divided by 2.
+eg:2,4,6,8,10,...
+
+odd numbers:
+A number that is not divisible by 2 and leaves a remainder '1'.
+eg:1,3,5,7....
+
+Prime numbers:
+Prime numbers are the positive integers having only two factors, 1 and the integer itself.
+
+eg:2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,\
+      107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,\
+      227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347,\
+      349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,\
+      479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617,\
+      619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, \
+      769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883,\
+      887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997.
+
+composite numbers:
+composite numbers are numbers that have more than two factors. These numbers are also called composites.
+eg:4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44,\
+      45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77, 78, 80,\
+      81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 100, 102, 104, 105, 106, 108, 110, 111, 112, 114,\
+      115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 128, 129, 130, 132, 133, 134, 135, 136, 138, 140, 141,\
+      142, 143, 144, 145, 146, 147, 148, 150,  152, 154, 155, 156, 157, 158, 159, 160, 162, 164, 165, 166, 168, 170, 172, \
+      174, 175, 176, 177, 178, 180, 182,184, 185, 186, 187, 188, 189, 190, 192, 194, 195, 196, and 198.
+
+complex numbers:
+are the numbers that are expressed in the form of a+ib where, a,b are real numbers and  ‘i’ is
+an imaginary number called “iota”. The value of i = (√-1).
+For example, 2+3i is a complex number, where 2 is a real number (Re) and 3i is an imaginary number (Im).
+
+Real numbers:
+combination of rational numbers and irrational numbers.
+
+
+Data Types:
+
+Text Type:	str eg:'srilekha'
+Numeric Types:	int (eg: 4,5,..) , float (eg: 0.3,7.9...), complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset(eg:x = frozenset({"apple", "banana", "cherry"}))
+Boolean Type:	bool (True,False)
+Binary Types:	bytes, bytearray, memoryview
+
+
+
+Conversion: converting from one unit to another unit ....one data type to another datatype
+
+Temperature:
+
+1 celsius =33.8 Fahrenheit
+
+Length:
+
+Length:
+            1 km = 1000 m
+            1 m  = 100 cm
+            1 cm = 10 mm    ==> 1KM + 100M ==>  1KM + 0.1KM = 1.1 KM**
+                                               1000M + 100M = 1100 M
+
+
+
+
+
+'''
